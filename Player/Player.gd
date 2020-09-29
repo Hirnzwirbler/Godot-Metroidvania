82f4d49680d3dc923672b4fcd03c7bfbc76573ec
Hirnzwirbler <fortner.steffen@web.de>
2020-09-29 17:40:42 +0200

    Lesson 7

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,11 +9,13 @@ export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
+var just_jumped = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 func _physics_process(delta):
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
@@ -45,13 +47,14 @@ func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			just_jumped = true
 			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE / 2:
 			motion.y = -JUMP_FORCE / 2
 
 func apply_gravity(delta):
-	#if not is_on_floor():
+	if not is_on_floor():
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
 
@@ -66,4 +69,22 @@ func update_animations(input_vector):
 		spriteAnimator.play("Jump")
 
 func move():
-	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4, MAX_SLOPE_ANGLE)
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_position = position
+	var last_motion = motion
+	
+	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	
+	#Landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	#Just left Ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
+	
+	#Prevent Sliding (Hack)
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
+		position.x = last_position.x
