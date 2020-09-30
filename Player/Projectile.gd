9964684cba71047975d62d23fece91b74401a3a7
Hirnzwirbler <fortner.steffen@web.de>
2020-09-30 08:11:29 +0200

    Lesson 13

--- Player/Projectile.gd
+++ Player/Projectile.gd
@@ -6,5 +6,5 @@ func _process(delta):
 	position += velocity * delta
 
 
-func _on_VisibilityNotifier2D_viewport_exited(viewport):
+func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
