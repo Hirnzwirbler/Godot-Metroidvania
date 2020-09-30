1c59494967e7e59a3f4cbef23c53cee817043354
Hirnzwirbler <fortner.steffen@web.de>
2020-09-30 20:01:35 +0200

    Lesson 23

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -5,10 +5,12 @@ var max_health = 4
 var health = max_health setget set_health
 
 signal player_died
+signal player_health_changed
 
 func set_health(value):
 	if value < health:
 		Events.emit_signal("add_screenshake", 0.25, 0.5)
 	health = clamp(value, 0, max_health)
+	emit_signal("player_health_changed", health)
 	if health == 0:
 		emit_signal("player_died")
